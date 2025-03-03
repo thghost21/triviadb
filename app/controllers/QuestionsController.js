772acd08ef9754f0902d91fb13a43fda29b59e50
thghost21 <tylerhacker21@gmail.com>
2025-03-03 15:44:13 -0700

    got all answers

--- app/controllers/QuestionsController.js
+++ app/controllers/QuestionsController.js
@@ -1,10 +1,13 @@
+import { AppState } from "../AppState.js";
 import { questionsService } from "../services/QuestionsService.js";
+
+
 
 
 export class QuestionsController {
 
   constructor(parameters) {
-
+    AppState.on('questions', this.drawQuestions)
   }
 
   async getQuestions() {
@@ -17,6 +20,16 @@ export class QuestionsController {
       console.error('Could not retrieve questions from API', error);
 
     }
+
+  }
+
+  drawQuestions() {
+    const questions = AppState.questions
+    let questionsCardContent = ''
+    questions.forEach(question => questionsCardContent += question.questionCard)
+    const questionsCardElem = document.getElementById('questionCardArea')
+    questionsCardElem.innerHTML = questionsCardContent
+
 
   }
 
--- app/models/Question.js
+++ app/models/Question.js
@@ -6,7 +6,52 @@ export class Question {
     this.question = data.question
     this.correctAnswer = data.correct_answer
     this.incorrectAnswers = data.incorrect_answers
+
+  }
+
+  get allAnswersForButtons() {
+    let allAnswers = []
+    allAnswers.push(this.incorrectAnswers)
+    allAnswers.push(this.correctAnswer)
+    return allAnswers
   }
 
 
+
+
+  get guessesButtons() {
+    if (this.type == 'boolean') {
+      return `<div class="mb-2">
+                <button role="button" class="col-6 btn btn-primary">${this.allAnswersForButtons}</button>
+              </div>
+              <div class="mb-2">
+                <button role="button" class="col-6 btn btn-primary">${this.incorrectAnswers}</button>
+              </div>`
+    }
+    return
+  }
+
+
+  get questionCard() {
+    return `
+    <div class="col-6 my-3">
+          <div class="bg-primary rounded text-light textShadow">
+            <span>Question 1: ${this.category}</span>
+            <span class="ms-2">${this.difficulty}</span>
+          </div>
+          <div class="shadow">
+            <div>
+              <p>${this.question}</p>
+            </div>
+            <div class="d-flex justify-content-between px-2 flex-column text-center">
+              ${this.guessesButtons}
+            </div>
+
+          </div>
+        </div>
+    `
+  }
 }
+//<div class="mb-2">
+//   <button role="button" class="col-6 btn btn-primary">Answer 1</button>
+// </div>
